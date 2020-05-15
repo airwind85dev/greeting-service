037f73ade717490a5bf4dfb2f1de8006bf75fd80
Aravind <airwind.dev@gmail.com>
2020-05-16 01:06:15 +0530

    swagger addition

--- greeting-service/src/main/java/com/home/greetings/greetingservice/GreetingServiceApplication.java
+++ greeting-service/src/main/java/com/home/greetings/greetingservice/GreetingServiceApplication.java
@@ -2,12 +2,48 @@ package com.home.greetings.greetingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class GreetingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingServiceApplication.class, args);
 	}
+
+	@Bean
+	Docket configureSwagger(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/base/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.home.greetings.greetingservice"))
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo(){
+		return new ApiInfo("Greetings API",
+				"Sample Greetings service",
+				"1.0",
+				"Terms - Free to use!!",
+				new Contact
+						("Aravind",
+								"https://github.com/ariwind-home/greeting-service",
+								"airwind85.dev@gmail.com"),
+				"API License",
+				"https://github.com/ariwind-home/greeting-service",
+				Collections.emptyList());
+	}
+
 
 }
--- greeting-service/src/main/java/com/home/greetings/greetingservice/controller/GreetingController.java
+++ greeting-service/src/main/java/com/home/greetings/greetingservice/controller/GreetingController.java
@@ -1,7 +1,9 @@
 package com.home.greetings.greetingservice.controller;
 
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -9,13 +11,18 @@ import java.util.List;
 import java.util.Random;
 
 @RestController
+@RequestMapping("/base")
 public class GreetingController {
 
+    @ApiOperation(value = "Greets with a Hi! all the time", notes = "hit with a / to get a 'Hi!' message",
+            response = String.class)
     @GetMapping("/")
     public String home() {
         return "Hi!";
     }
 
+    @ApiOperation(value = "Greets with static values", notes = "hit with a /greeting to get a random greeting message",
+    response = String.class)
     @GetMapping("/greeting")
     public String greet(){
         List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
